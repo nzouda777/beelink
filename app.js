621ad652b1da9ed0ba31df85542ffe1016be82ad
nzouda777 <nzoudamartial@gmail.com>
2022-12-29 13:41:56 +0100

    maj

---
+++ app.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  
+});
+Footer
